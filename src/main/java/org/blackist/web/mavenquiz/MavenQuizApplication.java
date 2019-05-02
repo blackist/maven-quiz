fe113f1cade3d514679fd072c43913b10e14b866
LiangLiang.Dong <liangl.dong@gmail.com>
2019-05-02 16:40:14 +0800

    [Maven] init repo

---
+++ src/main/java/org/blackist/web/mavenquiz/MavenQuizApplication.java
@@ -0,0 +1,13 @@
+package org.blackist.web.mavenquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenQuizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenQuizApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/blackist/web/mavenquiz/web/MainController.java
@@ -0,0 +1,15 @@
+package org.blackist.web.mavenquiz.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+
+    @GetMapping
+    public String index() {
+        return "Maven SpringBoot!";
+    }
+}
